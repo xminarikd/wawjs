913c99dcccd5fbe18646f4ac5d5ed0bbf6c50090
Daniel Minarik <dannyel.minarik@gmail.com>
2019-10-21 14:26:57 +0200

    praca na cviceni

--- 04-cvicenie/ulohy/src/introspect.js
+++ 04-cvicenie/ulohy/src/introspect.js
@@ -14,49 +14,51 @@ module.exports = {
   hasInheritedProperty,
   hasOverridenProperty
 };
-// Object.keys supporting Symbols and non-enumerables 
+// Object.keys supporting Symbols and non-enumerables
 function allOwnKeys(o) {
-  
+  let prop = Object.getOwnPropertyNames(o);
+  let sym = Object.getOwnPropertySymbols(o);
+  //console.log(prop.concat(sym));
+  return prop.concat(sym);
 }
-// Object.values supporting Symbols and non-enumerables 
+// Object.values supporting Symbols and non-enumerables
 function allOwnValues(o) {
-  
+  //console.log(allOwnKeys(o).map(key => o[key]));
+  return allOwnKeys(o).map(key => o[key]);
 }
-// Object.entries supporting Symbols and non-enumerables 
+// Object.entries supporting Symbols and non-enumerables
 function allOwnEntries(o) {
-  
+  return allOwnKeys(o).map(key => [key, o[key]]);
 }
 // [obj,...protos] array of objects in proto chain
 // starting with obj itself and up-the chain
 function getProtoChain(obj) {
-  
+  let arr = [obj];
+  while ((obj = Object.getPrototypeOf(obj))) {
+    arr.push(obj);
+  }
+  return arr;
 }
-// Object.keys including, inherited, not-enumeble, symbols  
-function allKeys(obj) {
-  
-}
+// Object.keys including, inherited, not-enumeble, symbols
+function allKeys(obj) {}
 
 // for..in loop supporting Symbols and non-enumerable
 // for own and inherited properties
-function forIn(obj, callback) {
-  
-}
-// create copy of object 
-// with same propereties, 
-// including symbols, 
-// same values 
-// and same property ownership 
+function forIn(obj, callback) {}
+// create copy of object
+// with same propereties,
+// including symbols,
+// same values
+// and same property ownership
 function shallowClone(obj) {
-  
+  return Object.create(
+    Object.getPrototypeOf(obj),
+    Object.getOwnPropertyDescriptors(obj)
+  );
 }
 
 // if the property exists only in proto chain
 // not on object
-function hasInheritedProperty(obj, prop) {
-  
-}
+function hasInheritedProperty(obj, prop) {}
 
-function hasOverridenProperty(obj, prop) {
-  
-
-}
+function hasOverridenProperty(obj, prop) {}
--- 04-cvicenie/ulohy/test/creating-objects.spec.js
+++ 04-cvicenie/ulohy/test/creating-objects.spec.js
@@ -1,20 +1,19 @@
 const assert = require("assert");
 
 describe("Creating objects", function() {
-
   it("01 - Basic properties and metods", function() {
-
     let o = {
-      // create object with property and method
-      // use method shortcut syntax
-      // TODO:
-      //
-      //
-     
+      a: "",
+      b() {
+        return this.a;
+      }
     };
 
-    assert(o.hasOwnProperty("a") && o.hasOwnProperty("b"), "Object has two properties");
-    assert(typeof o.b === "function")
+    assert(
+      o.hasOwnProperty("a") && o.hasOwnProperty("b"),
+      "Object has two properties"
+    );
+    assert(typeof o.b === "function");
     assert(o.b() === o.a, "b is method return value of a");
 
     o.a = Math.random();
@@ -23,41 +22,63 @@ describe("Creating objects", function() {
   it("02 - getter", function() {
     // same as previous just make b getter instead of function
     let o = {
-      // create object with property and method
-      // use method shortcut syntax
-      // TODO:
-      //
-      //
-      
+      a: "",
+      get b() {
+        return this.a;
+      }
     };
 
-    assert(o.hasOwnProperty("a") && o.hasOwnProperty("b"), "Object has two properties");
+    assert(
+      o.hasOwnProperty("a") && o.hasOwnProperty("b"),
+      "Object has two properties"
+    );
     assert(o.b === o.a, "b is property and return value of a");
 
     o.a = Math.random();
     assert(o.b === o.a);
   });
   it("03 - non enumerable properties", function() {
-    // create object with property "a" "hidden" 
+    // create object with property "a" "hidden"
     // and accessible, only with getter b
     // b shall be visible (enumerable)
     let o; //TODO: use Object.create...
-    
 
-    assert.deepStrictEqual(o && Object.keys(o), ["b"], "shall have ONE enumerable property b")
+    o = Object.create(null, {
+      a: {
+        value: 10
+      },
+      b: {
+        get() {
+          return this.a;
+        },
+        enumerable: true
+      }
+    });
+
+    assert.deepStrictEqual(
+      o && Object.keys(o),
+      ["b"],
+      "shall have ONE enumerable property b"
+    );
     assert(o.b === o.a, "b is property and return value of a");
     o.a = Math.random();
     assert(o.b === o.a);
   });
   it("04 - inheritance with Object.create", function() {
-
     let base = {
       get b() {
         return this.a;
       }
     };
-    let o; // TODO: inherit from base using Object.create... 
-    
+    let o; // TODO: inherit from base using Object.create...
+
+    o = Object.create(base, {
+      a: {
+        value: 10,
+        enumerable: true,
+        writable: true
+      }
+    });
 
     assert(typeof o === "object");
     assert("b" in o && "a" in o, "shall have properties a,b");
@@ -68,11 +89,11 @@ describe("Creating objects", function() {
     let changed = Math.random();
     o.a = changed;
     assert(o.b === changed, "a shall be writable");
-
   });
   it("05 - create object not inherited from anything", function() {
     let o; //TODO:
-    
+
+    o = Object.create(null);
 
     assert(typeof o === "object");
     assert(Object.getPrototypeOf(o) === null);
@@ -80,25 +101,46 @@ describe("Creating objects", function() {
   it("06 - modify API of array instance", function() {
     let arr = ["a", "b", "c"];
     arr.push(666);
-    assert.deepStrictEqual(arr, ["a", "b", "c", 666], "this is standard behavior");
+    assert.deepStrictEqual(
+      arr,
+      ["a", "b", "c", 666],
+      "this is standard behavior"
+    );
 
     function typedArray(arr) {
-
-      return arr;
+      return Object.defineProperty(arr, "push", {
+        value: function(...args) {
+          args = args.filter(s => typeof s == "string");
+          return Array.prototype.push.apply(this, args);
+        }
+      });
     }
     let orig = ["a", "b", "c"];
     let arr2 = typedArray(orig);
     arr2.push(666); //will silently ignore
     arr2.push({}); //will silently ignore
-    assert.deepStrictEqual(arr2, ["a", "b", "c"], "typed array wil silently ignore non string value");
-    assert(arr2 === orig, "Shell be augmented instance not wraper, not inherited");
+    assert.deepStrictEqual(
+      arr2,
+      ["a", "b", "c"],
+      "typed array wil silently ignore non string value"
+    );
+    assert(
+      arr2 === orig,
+      "Shell be augmented instance not wraper, not inherited"
+    );
     arr2.push("d", "e", "f");
-    assert.deepStrictEqual(arr2, ["a", "b", "c", "d", "e", "f"], "beware: push may have multiple args");
-    assert.deepStrictEqual(Object.keys(arr2), ["0", "1", "2", "3", "4", "5"], "beware: push may have multiple args");
-
+    assert.deepStrictEqual(
+      arr2,
+      ["a", "b", "c", "d", "e", "f"],
+      "beware: push may have multiple args"
+    );
+    assert.deepStrictEqual(
+      Object.keys(arr2),
+      ["0", "1", "2", "3", "4", "5"],
+      "beware: push may have multiple args"
+    );
   });
   it("08 - create objects with constructors", function() {
-
     let base = {
       get b() {
         return this.a;
@@ -106,38 +148,39 @@ describe("Creating objects", function() {
     };
     //TODO: create object with own "a" and  "b" from prototype
     function MyObject(a) {
-      // TODO: define own property
-     
+      this.a = a;
     }
+    MyObject.prototype = base;
     // TODO: define inherited properties
-   
 
     let o = new MyObject("test");
     assert("b" in o && "a" in o, "shall have properties a,b");
     assert(o.hasOwnProperty("a"), "a shell be own");
-    assert(!(o.hasOwnProperty("b")), "b shell be inherited");
+    assert(!o.hasOwnProperty("b"), "b shell be inherited");
     // beware
     assert(Object.getPrototypeOf(o) === base);
     assert(Object.getPrototypeOf(o).hasOwnProperty("b"));
-
   });
 
   it("09 - create objects with classes", function() {
     // same as previous just use class syntax
     // create object with own "a" and  "b" from prototype
     class MyObject {
-      // TODO: 
-      
+      // TODO:
+      constructor(a) {
+        this.a = a;
+      }
+      get b() {
+        return this.a;
+      }
     }
-
 
     let o = new MyObject("test");
     assert("b" in o && "a" in o, "shall have properties a,b");
     assert(o.hasOwnProperty("a"), "a shell be own");
-    assert(!(o.hasOwnProperty("b")), "b shell be inherited");
+    assert(!o.hasOwnProperty("b"), "b shell be inherited");
     // beware
     assert.equal(Object.getPrototypeOf(o).constructor, MyObject);
     assert(Object.getPrototypeOf(o).hasOwnProperty("b"));
-  })
-
+  });
 });
